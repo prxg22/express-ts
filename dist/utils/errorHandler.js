c31b0feb9ef9ed8568480438450580b1f19d9f2b
prxg22 <prxg22@gmail.com>
2017-07-09 21:27:27 -0300

    gitignore

--- dist/utils/errorHandler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = function (code, err, res) {
-    console.log(err.message + '\n');
-    return res.status(code).send(err);
-};
