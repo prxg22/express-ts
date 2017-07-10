a3338eef484c624592a10c6fdf86e6869ac80524
PauloRicardo <prxg22@gmail.com>
2017-07-10 10:16:58 -0300

    Registro de parametros para rota.

--- src/route.ts
+++ src/route.ts
@@ -2,32 +2,49 @@ import { Router, Request, Response, NextFunction } from 'express';
 import { Connection } from 'mongoose';
 import { Error, ErrorCode } from './error';
 
-export class Route {
+export enum ParamSource {
+	query = 0,
+	body,
+	params
+}
+export class Param {
+	constructor(public name?: string, public source?: ParamSource, private type?: any) {
+		if (this.source < 0) {
+			this.source = ParamSource.body;
+		}
+	};
+	
+	public get(req: Request): any {
+		let source: string = ParamSource[this.source];
+		let res: any = req[source];
+
+		if (this.name) {
+			res = res[this.name];
+		}
+		
+		if(this.type) {
+			res = new this.type(res);
+		}
+
+		return res;
+	}
+}
+
+export class RouteRegistration {
 	constructor(
 		public path: string, 
 		public method: string, 
 		public action: any,
-		public params?: string[]
+		public params?: Param[]
 	) {};
 
-	public useParams(): boolean {
-		let regex: RegExp = /:\w+/g;
-
-		if (!this.path) {
-			return false;
-		}
-
-		let params = this.path.match(regex);
-		if (!params || params.length < 1) {
-			return false;
-		}
-
-		return true;
+	public getParams(req: Request) {
+		return this.params.map((param: Param) => param.get(req));
 	}
 }
 
-export class Controller<T> {
-	public routes: Route[];
+export class Route<T> {
+	public routes: RouteRegistration[];
 
 	constructor(private path: string, private router: Router, protected connection?: Connection) {
 		this.routes = [];
@@ -45,15 +62,13 @@ export class Controller<T> {
 
 	protected registerRoutes(){};
 
-	private get(route: Route) {
+	private get(route: RouteRegistration) {
 		return (req: Request, res: Response, next: NextFunction) => {
-			let params = route.useParams();
+			let params = route.getParams(req);
+			let action = this[route.action];
+
 			let promise: Promise<T|T[]>;
-			if (params) {
-				promise = this[route.action](req.params, req.query);
-			} else {
-				promise = this[route.action](req.query);
-			}
+			promise = this[route.action](...params);
 
 			return promise.then(
 				(results: T|T[]) => results ? res.send(results) : new Error(ErrorCode.NOT_FOUND),
--- src/type/type.ts
+++ src/type/type.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { Connection, Model } from 'mongoose';
 
-import { Route, Controller } from '../route';
+import { RouteRegistration, Route, Param, ParamSource } from '../route';
 import { TypeSchema, TypeModel } from './type.schema';
 
 export interface Type {
@@ -12,26 +12,31 @@ export interface Type {
 }
 
 
-export class TypeController extends Controller<Type> {
+export class TypeController extends Route<Type> {
 	constructor(router: Router, connection: Connection) {
 		super('/type', router, connection);
 	}
 
 	public getAll(query: any): Promise<Type[]> {
-		console.log('entrei sem parametros');
 		let model = this.connection.model<TypeModel>('type', TypeSchema);
+		console.log(query);
 		return model.find();
 
 	}
 
-	public getOne(params: any, query: any): Promise<Type[]> {
-		console.log(params);
+	public getOne(id: string, query: any): Promise<Type> {
 		let model = this.connection.model<TypeModel>('type', TypeSchema);
-		return model.find({_id: params.id});
+		return model.findOne({_id: id});
 	}
 
 	protected registerRoutes() {
-		this.routes.push(new Route('/', 'get', 'getAll'));
-		this.routes.push(new Route('/:id', 'get', 'getOne'));
+		this.routes.push(new RouteRegistration('/', 'get', 'getAll', [
+			new Param('modifiedAt', ParamSource.query, Date);
+		]));
+
+		this.routes.push(new RouteRegistration('/:id', 'get', 'getOne', [
+			new Param('id', ParamSource.params),
+			new Param('', ParamSource.query)
+		]));
 	}
 }
